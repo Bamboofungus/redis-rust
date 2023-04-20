2088021a0f2695d86c8b90f1aca869d1c3caea5f
Bamboofungus <anthony@ambii.com>
2023-04-20 15:46:56 +0900

    handle multiple connections

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,49 @@
 // Uncomment this block to pass the first stage
-use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write};
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+// use std::net::{TcpListener, TcpStream};
+// use std::io::{Read, Write};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+#[tokio::main]
+async fn main() {
     println!("Logs from your program will appear here!");    
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => {
-                println!("accepted new connection");
-                handle_stream(_stream);
-            }
+    // TODO handle error
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    loop {
+        let (stream, _) = match listener.accept().await {
+            Ok((_stream, addr)) => (_stream, addr),
             Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+                eprintln!("Couldn't accept client: {:?}", e);
+                break
+            },
+        };
+        handle_stream_with_task(stream);
     }
 }
 
-fn handle_stream(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    loop {
-        match stream.read(&mut buffer) {
-            Ok(n) => {
-                // end of stream
-                if n == 0 {
-                    break
+fn handle_stream_with_task(mut stream: TcpStream) {
+    // create task to handle each stream opened
+    tokio::task::spawn(async move {
+        let mut buffer = [0; 1024];
+        loop {
+            match stream.read(&mut buffer).await {
+                Ok(0) => {
+                    // conn closed
+                    println!("Stream closed");
+                    break;
+                },
+                Ok(n) => {
+                    let message = &buffer[..n];
+                    println!("Received: {:?}", String::from_utf8_lossy(message));
+                    let res = "+PONG\r\n";
+                    stream.write_all(res.as_bytes()).await.unwrap();
+                },
+                Err(_) => {
+                    eprintln!("Error reading from stream: {:?}", stream);
+                    break;
                 }
-                let res = "+PONG\r\n";
-
-                stream.write_all(res.as_bytes()).unwrap();
-                // clear buffer
-                buffer =  [0; 1024]
-            },
-            Err(e) => {
-                println!("Error: {}", e);
-                break;
             }
-        }
-    }
-
+            buffer = [0; 1024];
+        }        
+    });   
 }
